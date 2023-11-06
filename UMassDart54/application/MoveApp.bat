db45d5ff64641b5807c3c4f07730ef8d3b99046c
dramsamooj2099 <99836337+dramsamooj2099@users.noreply.github.com>
2023-11-05 22:12:14 -0500

    Add more scenarios

---
+++ UMassDart54/application/MoveApp.bat
@@ -0,0 +1,3 @@
+COPY /Y "C:\MOSAIC\Applications\VehicleCommunicationApp1\target\VehicleCommunicationApp1-0.0.1.jar" .
+COPY /Y "C:\MOSAIC\Applications\RSUComApp52\target\RSUComApp52-0.0.1.jar" .
+COPY /Y "C:\MOSAIC\Applications\VehicleSendApp2\target\VehicleSendApp2-0.0.1.jar" .
